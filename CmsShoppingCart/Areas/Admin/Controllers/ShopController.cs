942733619226278691bb956657fab57dec60c16e
Pharaoh <pharaohmanson@gmail.com>
2019-09-13 22:15:20 -0400

    added featurae to add categories

---
+++ CmsShoppingCart/Areas/Admin/Controllers/ShopController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CmsShoppingCart.Areas.Admin.Models;
+using CmsShoppingCart.Areas.Admin.Models.ViewModels.Shop;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Admin/Shop/Categories
+        public ActionResult Categories()
+        {
+            //Declare a list of models
+            List<CategoryVM> categoryVMList;
+
+            using (Db db = new Db())
+            {
+                //Init the list
+                categoryVMList = db.Categories
+                    .ToArray()
+                    .OrderBy(x => x.Sorting)
+                    .Select((x => new CategoryVM(x)))
+                    .ToList();
+
+            }
+
+            
+
+            //Rreturn view with list
+            return View(categoryVMList);
+        }
+    }
+}
--- CmsShoppingCart/Areas/Admin/Models/Db.cs
+++ CmsShoppingCart/Areas/Admin/Models/Db.cs
@@ -11,5 +11,6 @@ namespace CmsShoppingCart.Areas.Admin.Models
     {
         public DbSet<PageDTO> Pages { get; set; }
         public DbSet<SidebarDTO> Sidebar { get; set; }
+        public DbSet<CategoryDTO> Categories { get; set; }
     }
 }
