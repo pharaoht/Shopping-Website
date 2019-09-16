a16818809eeae7ed81f7db3cee4e4706e7ebc800
Pharaoh <pharaohmanson@gmail.com>
2019-09-16 09:38:30 -0400

    Added categories page functionality

--- CmsShoppingCart/Areas/Admin/Controllers/ShopController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/ShopController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CmsShoppingCart.Areas.Admin.Models;
+using CmsShoppingCart.Areas.Admin.Models.Data;
 using CmsShoppingCart.Areas.Admin.Models.ViewModels.Shop;
 
 namespace CmsShoppingCart.Areas.Admin.Controllers
@@ -26,11 +27,86 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
                     .ToList();
 
             }
-
-            
-
             //Rreturn view with list
             return View(categoryVMList);
         }
+        //Post: Admin/Shop/AddNewCategory
+        [HttpPost]
+         public string AddNewCategory(string catName)
+         {
+            //Declare id
+            string  id;
+
+            using (Db db = new Db())
+            {
+
+                //Check that the category name is unique
+                if (db.Categories.Any(x => x.Name == catName))
+                    return "titletaken";
+
+                //Init Dto
+                CategoryDTO dto = new CategoryDTO();
+
+                //Add to Dto
+                dto.Name = catName;
+                dto.Slug = catName.Replace(" ", "-").ToLower();
+                dto.Sorting = 100;
+
+                //Save Dto
+                db.Categories.Add(dto);
+                db.SaveChanges();
+
+                //get the id
+                id = dto.Id.ToString();
+            }
+
+            //Return the id
+            return id;
+         }
+
+         //Post: Admin/Shop/ReorderCategories
+         [HttpPost]
+         public void ReorderCategories(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                //Set initial count
+                int count = 1;
+
+                //Declare PageDto
+                CategoryDTO dto;
+
+                //Set sorting for each category
+                foreach (var catId in id)
+                {
+                    dto = db.Categories.Find(catId);
+                    dto.Sorting = count;
+
+                    db.SaveChanges();
+                    count++;
+
+                }
+            }
+        }
+         //Get:Admin/Shop/DeleteCategory/id
+         public ActionResult DeleteCategory(int id)
+         {
+             using (Db db = new Db())
+             {
+                 //Get the Category
+                 CategoryDTO dto = db.Categories.Find(id);
+
+                 //remove the Category
+                 db.Categories.Remove(dto);
+
+                 //save
+                 db.SaveChanges();
+
+
+
+             }
+             //redirect
+             return RedirectToAction("Categories");
+         }
     }
 }
